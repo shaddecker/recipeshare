8955114a76ebce14177bbd941c475eba33c698a7
shaddecker <shaddecker5@gmail.com>
2020-08-03 09:34:39 -0500

    relationships added to models

--- models/recipedetails.js
+++ models/recipedetails.js
@@ -10,7 +10,7 @@ module.exports = (sequelize, DataTypes) => {
      * The `models/index` file will call this method automatically.
      */
     static associate(models) {
-      // define association here
+      RecipeDetails.belongsTo(models.RecipeHeader, { foreignKey: "recipeId" });
     }
   };
   RecipeDetails.init({
